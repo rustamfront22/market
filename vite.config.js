f073e4a305417eba8ed5327632249f5b200f62f4
rustam <rustam.front@gmail.com>
2023-10-10 14:22:41 +0500

    init project

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-module.exports = defineConfig({
-  publicPath: '/todolist/'
-})
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
